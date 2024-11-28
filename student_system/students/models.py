544c485faad72522684ab90d2567f36d12cc2e97
diogo-dantas <diogo.dantas@live.com>
2024-11-28 10:19:14 -0300

    Criado o projeto, configurado o app e criada a classe Student.

---
+++ student_system/students/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField(unique=True)
+    enrollment_date = models.DateField(auto_now_add=True)
+    classroom = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
